5582c2dad7f742fe7c3f90a8a7d0c51eae52aafd
alexsandro costa Sodre <alexsandrosodre22@gmail.com>
2022-02-27 22:27:49 -0300

    organizaçao

---
+++ entrada/views.py
@@ -0,0 +1,8 @@
+from django import views
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+# Create your views here.
+def post_list(request):
+    return render(request, 'blog/post_list.html', {})
